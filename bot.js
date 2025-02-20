07ce0bf918d142ddbd4fc451a4cf6a5899bdc614
Mark202112 <94757972+Mark202112@users.noreply.github.com>
2025-02-20 16:23:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 const TOKEN = '7438793473:AAEGzUebSyhLxDmkROksOZSNK1HgTH6kU2c';
-const GAME_URL = 'https://yourgame.vercel.app/';
+const GAME_URL = 'https://mark202112.github.io/xflow.github.io/';
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
