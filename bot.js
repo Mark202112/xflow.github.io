bfad567e671f2f02f9d33b02a1b87d709e381713
Mark202112 <94757972+Mark202112@users.noreply.github.com>
2025-02-20 16:21:46 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,14 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const TOKEN = '7438793473:AAEGzUebSyhLxDmkROksOZSNK1HgTH6kU2c';
+const GAME_URL = 'https://yourgame.vercel.app/';
+
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendGame(msg.chat.id, 'X-FLOW');
+});
+
+bot.on('callback_query', (query) => {
+    bot.answerCallbackQuery(query.id, { url: GAME_URL });
+});
